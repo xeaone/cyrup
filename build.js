529dc1e4f7e79c74b8f19e80ac4652e659cdc7d6
Alexander Elias <alex.steven.elias@gmail.com>
2018-06-13 16:27:43 -0700

    updated build file

--- build.js
+++ build.js
@@ -1,5 +1,9 @@
+
+const Cp = require('child_process');
+const Mp = Cp.execSync('npm root -g').toString().trim();
+const Muleify = require(`${Mp}/muleify`);
+
 const Package = require('./package');
-const Muleify = require('muleify');
 const Util = require('util');
 const Fs = require('fs');
 
@@ -7,14 +11,17 @@ const ReadFile = Util.promisify(Fs.readFile);
 const WriteFile = Util.promisify(Fs.writeFile);
 
 const name = Package.name;
+const email = Package.email;
+const author = Package.author;
+const license = Package.license;
 const version = Package.version;
 
 const header = `/*
 	Name: ${name}
 	Version: ${version}
-	License: MPL-2.0
-	Author: Alexander Elias
-	Email: alex.steven.elias@gmail.com
+	License: ${license}
+	Author: ${author}
+	Email: ${email}
 	This Source Code Form is subject to the terms of the Mozilla Public
 	License, v. 2.0. If a copy of the MPL was not distributed with this
 	file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -28,10 +35,18 @@ const prepend = async function (data, path) {
 
 (async function () {
 
-	const options = { minify: true };
+	const options = {
+		bundle: true,
+		transpile: true,
+	};
 
-	await Muleify.pack('src/cyrup.js', 'dis/cyrup.min.js', options);
-	await prepend(header, 'dis/cyrup.min.js');
+	await Muleify.pack(`src/${name}.js`, `dis/${name}.js`, options);
+	await prepend(header, `dis/${name}.js`);
+
+	options.minify = true;
+
+	await Muleify.pack(`src/${name}.js`, `dis/${name}.min.js`, options);
+	await prepend(header, `dis/${name}.min.js`);
 
 }()).catch(function (error) {
 	console.error(error);
